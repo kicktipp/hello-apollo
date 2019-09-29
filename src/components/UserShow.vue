f6e8326c6b08f39da9bbb65259dea4c4b75c4d88
Janning Vygen <vygen@kicktipp.de>
2019-09-29 22:32:15 +0200

    fix

---
+++ src/components/UserShow.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="!this.$apollo.queries.user.loading">
+        Your name is: {{user.firstname}}
+    </div>
+</template>
+
+<script>
+
+    import userQuery from "./user";
+
+    export default {
+        apollo: {
+            user: {
+                query: userQuery
+            }
+        }
+    }
+</script>
+
